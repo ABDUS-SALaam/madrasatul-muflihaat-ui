b6300ddc22d9058af5203db2ca1604bdd2f5c619
mehtaabnazar <mehtaab.nazar@gmail.com>
2025-11-13 01:13:07 +0530

    removed imports

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 
-// Import global CSS libraries (works with ESBuild in Angular 17+)
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'animate.css/animate.min.css';
-import 'aos/dist/aos.css';
-import 'aos/dist/aos.js';
+// Import global CSS libraries (works with ESBuild in Angular 17+
 
 bootstrapApplication(AppComponent)
   .catch(err => console.error(err));
